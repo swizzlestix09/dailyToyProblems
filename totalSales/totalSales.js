9b80398f8f76edfb831cb440dab6795b99e6f9e0
swizzlestix09 <angely.rodriguez@gmail.com>
2021-09-21 21:01:13 -0400

    SA review for week 0 complete

--- totalSales/totalSales.js
+++ totalSales/totalSales.js
@@ -13,32 +13,32 @@
 //
 // EXAMPLE:
 //
-// var salesTeam = {
-//   name: 'Arnaldo McDermott',
-//   individualSales: 14,
-//   leadsInProgress: 10,
-//   manages: [
-//     {
-//       name: 'Lavina Romaguera',
-//       individualSales: 15,
-//       leadsInProgress: 22,
-//       manages: [
-//         {
-//           name: 'Glen Hodkiewicz',
-//           individualSales: 12,
-//           leadsInProgress: 10,
-//           manages: []
-//         }
-//       ]
-//     },
-//     {
-//       name: 'Rey Hills',
-//       individualSales: 19,
-//       leadsInProgress: 5,
-//       manages: []
-//     }
-//   ]
-// };
+var salesTeam = {
+  name: 'Arnaldo McDermott',
+  individualSales: 14,
+  leadsInProgress: 10,
+  manages: [
+    {
+      name: 'Lavina Romaguera',
+      individualSales: 15,
+      leadsInProgress: 22,
+      manages: [
+        {
+          name: 'Glen Hodkiewicz',
+          individualSales: 12,
+          leadsInProgress: 10,
+          manages: []
+        }
+      ]
+    },
+    {
+      name: 'Rey Hills',
+      individualSales: 19,
+      leadsInProgress: 5,
+      manages: []
+    }
+  ]
+};
 //
 //
 // totalSales(salesTeam)
@@ -48,5 +48,21 @@
 
 
 var totalSales = function (salesTeam) {
-  // All your code in this function body
+  //create total variable for return
+  var total = 0;
+  //iterate through object
+  for (var data in salesTeam) {
+    if (data === 'individualSales') {
+      total += salesTeam[data]
+    }
+  }
+
+  if (salesTeam['manages']) {
+    for (var i = 0; i < salesTeam['manages'].length; i++) {
+      total += totalSales( salesTeam['manages'][i] );
+    }
+  }
+  return total
 };
+
+console.log( totalSales(salesTeam) )
