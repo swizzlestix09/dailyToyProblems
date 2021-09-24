22b61f5f3a710e7d9c5dc0e619a8911cb0d378c1
swizzlestix09 <angely.rodriguez@gmail.com>
2021-09-24 14:56:34 -0400

    wtf with three sum

---
+++ threeSum/threeSum.js
@@ -0,0 +1,17 @@
+// Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+// Input: nums = [-1,0,1,2,-1,-4]
+// Output: [[-1,-1,2],[-1,0,1]]
+// Notice that the solution set must not contain duplicate triplets.
+
+// return all the triplets [nums[i], nums[j], nums[k]] such that
+// i != j,
+// i != k,
+// and j != k,
+// and nums[i] + nums[j] + nums[k] == 0.
+
+var threeSum = function (nums) {
+  var allResults = [];
+
+  return allResults;
+};
+console.log(threeSum([-1, 0, 1, 2, -1, -4])); //[[-1,-1,2],[-1,0,1]]
