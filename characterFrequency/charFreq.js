46053f1745e38ff317a9bbc079cd5f99a676537a
swizzlestix09 <angely.rodriguez@gmail.com>
2021-09-28 15:37:41 -0400

    char freq explanation complete

--- characterFrequency/charFreq.js
+++ characterFrequency/charFreq.js
@@ -34,7 +34,8 @@
  *
  * CharacterFrequency is a function that takes in a string as a parameter.
  * An object was created to hold the letters and the frequency in which the letters appear in the string.
- * Using a for loop, we iterate through the string. If the character is a space or number (using the string match method and regex to match numbers), we ignore it. Otherwise we check to see if the character
+ * Using a for loop, we iterate through the string. If the character is a space or number (using the string match method and regex to match numbers), we ignore it. Otherwise we check to see if the character is in the object, incrementing it if it is (and creating it if it isn't). After we create out object, we create an array of ararys with the key value pairs using Object.entries.
+ * We return the sorted array, sorted by the sort function. We pass in a function where we compare the numberical value in each array, ordering it by ascending values primarily, then using the .localeCompare method to compare letters by decending value.
  *
  */
 
