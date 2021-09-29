c21da0d4772ebd8aad8672fe76c06b4ba6a3f521
swizzlestix09 <angely.rodriguez@gmail.com>
2021-09-29 13:52:41 -0400

    longest pali tests started

---
+++ longestPali/longestPali.test.js
@@ -0,0 +1,25 @@
+const longestPali = require('./longestPali');
+
+test(`expected "My dad is a racecar athlete" to output "a racecar a"`, () => {
+  expect(longestPali("My dad is a racecar athlete")).toBe("a racecar a");
+});
+
+test(`expected "Excuse me Madam I'm Adam" to output "Madam I'm Adam"`, () => {
+  expect(longestPali("Excuse me Madam I'm Adam" )).toBe("Madam I'm Adam");
+});
+
+test(`expected "My dad is a racecar athlete" to output "a racecar a"`, () => {
+  expect(longestPali("My dad is a racecar athlete")).toBe("a racecar a");
+});
+
+test(`expected "My dad is a racecar athlete" to output "a racecar a"`, () => {
+  expect(longestPali("My dad is a racecar athlete")).toBe("a racecar a");
+});
+
+test(`expected "My dad is a racecar athlete" to output "a racecar a"`, () => {
+  expect(longestPali("My dad is a racecar athlete")).toBe("a racecar a");
+});
+
+test(`expected "My dad is a racecar athlete" to output "a racecar a"`, () => {
+  expect(longestPali("My dad is a racecar athlete")).toBe("a racecar a");
+});
