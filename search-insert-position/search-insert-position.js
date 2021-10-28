ae924052f912e580783576de4f47175d9ef827ae
Angely Rodriguez <34196367+swizzlestix09@users.noreply.github.com>
2021-10-27 21:15:03 -0400

    Time: 72 ms (83.16%), Space: 39.7 MB (43.69%) - LeetHub

--- search-insert-position/search-insert-position.js
+++ search-insert-position/search-insert-position.js
@@ -4,12 +4,6 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
-    if (nums[ nums.length - 1] < target) {
-        return nums.length
-    }
-    if (nums[0] > target) {
-        return 0
-    }
   let start = 0
   let end = nums.length - 1
   let mid;
