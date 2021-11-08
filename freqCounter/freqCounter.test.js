0f9663215a257bb1e907efaabb6b9a078f3a9819
swizzlestix09 <angely.rodriguez@gmail.com>
2021-11-08 13:27:16 -0500

    isSubsequence solved + tests

--- freqCounter/freqCounter.test.js
+++ freqCounter/freqCounter.test.js
@@ -1,13 +1,13 @@
-const duplicates = require('./freqCounter');
+const areThereDuplicates = require('./freqCounter');
 
 test(`expected 1, 2, 3 to output false`, () => {
-  expect(duplicates(1, 2, 3)).toBe(false);
+  expect(areThereDuplicates(1, 2, 3)).toBe(false);
 });
 
 test(`expected 1, 2, 2 to output false`, () => {
-  expect(duplicates(1, 2, 2)).toBe(true);
+  expect(areThereDuplicates(1, 2, 2)).toBe(true);
 });
 
 test(`expected 'a', 'b', 'c', 'a' to output true`, () => {
-  expect(duplicates('a', 'b', 'c', 'a' )).toBe(true);
+  expect(areThereDuplicates('a', 'b', 'c', 'a' )).toBe(true);
 });
