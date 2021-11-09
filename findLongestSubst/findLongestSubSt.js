2c4b8afcf7260c8ecfed57fd0ff895690b997ddb
swizzlestix09 <angely.rodriguez@gmail.com>
2021-11-09 14:13:43 -0500

    find longest sub string added w/ test pg

---
+++ findLongestSubst/findLongestSubSt.js
@@ -0,0 +1,10 @@
+/* find longest substring Colt Steele - DS & A course
+ * accepts a string and returns the length of the logest substring * with all distinct characters.
+
+*/
+
+const findLongestSubSt = () => {
+
+};
+
+module.exports = findLongestSubSt;
---
+++ findLongestSubst/findLongestSubSt.test.js
@@ -0,0 +1,1 @@
+var findLongestSubSt = require( './findLongestSubSt');
