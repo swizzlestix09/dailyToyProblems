b5ee44c362e17042ca7e0840a36ef47a29ca9ad5
swizzlestix09 <angely.rodriguez@gmail.com>
2021-11-09 21:52:28 -0500

    find longest - tests are being added

--- findLongestSubst/findLongestSubSt.js
+++ findLongestSubst/findLongestSubSt.js
@@ -24,10 +24,28 @@ const findLongestSubSt = (string) => {
       maxLeng = Math.max(maxLeng, word.length);
       word = '';
     }
-    console.log(word, maxLeng)
+
   }
   return maxLeng;
 };
+
+// function findLongestSubstring(str) {
+//   let longest = 0;
+//   let seen = {};
+//   let start = 0;
+
+//   for (let i = 0; i < str.length; i++) {
+//     let char = str[i];
+//     if (seen[char]) {
+//       start = Math.max(start, seen[char]);
+//     }
+//     // index - beginning of substring + 1 (to include current in count)
+//     longest = Math.max(longest, i - start + 1);
+//     // store the index of the next char so as to not double count
+//     seen[char] = i + 1;
+//   }
+//   return longest;
+// }
 
 console.log(findLongestSubSt('thisisawesome')); //6
 console.log(findLongestSubSt('longestsubstring')); //8
