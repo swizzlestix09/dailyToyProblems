0269e27e6dc03cb8c5d6d896deab45b5155a1a5b
Angely Rodriguez <34196367+swizzlestix09@users.noreply.github.com>
2021-11-15 14:23:20 -0500

    Time: 632 ms (5.01%), Space: 45.7 MB (76.74%) - LeetHub

--- squares-of-a-sorted-array/squares-of-a-sorted-array.js
+++ squares-of-a-sorted-array/squares-of-a-sorted-array.js
@@ -3,21 +3,55 @@
  * @return {number[]}
  */
 
+// var sortedSquares = function(nums) {
+//     let squares = [];
+//     let min = null; 
+//     let max = 0; 
+//     let j = 0;
+    
+//     nums.forEach(num => {
+//         let sq = Math.pow(num, 2)
+//         if (min === null || min > sq) {
+//             min = num
+//         }
+//         if (max < sq) {
+//             max = sq
+//         }
+//         squares.push(sq)
+//     })
+    
+//     return squares.sort( (a, b) => a-b);
+// };
+
+const sorted = (arr) => {
+  let isSorted = 1;
+  
+  while(isSorted > 0) {
+    isSorted = 0;
+    let temp;
+    for(let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i+1]) {
+        temp = arr[i]
+        arr[i] = arr[i+1]
+        arr[i+1] = temp
+        isSorted++;
+      }
+    }
+  }
+  
+  return arr;
+};
+
 var sortedSquares = function(nums) {
     let squares = [];
     let min = null; 
     let max = 0; 
     let j = 0;
+    
     nums.forEach(num => {
         let sq = Math.pow(num, 2)
-        if (min === null || min > sq) {
-            min = num
-        }
-        if (max < sq) {
-            max = sq
-        }
         squares.push(sq)
     })
     
-    return squares.sort( (a, b) => a-b);
+  return sorted(squares); 
 };
