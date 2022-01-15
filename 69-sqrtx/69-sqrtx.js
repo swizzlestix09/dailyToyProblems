89fdaded2ccc4e2c36765da494f544023fa51f94
Angely Rodriguez <34196367+swizzlestix09@users.noreply.github.com>
2022-01-15 15:24:53 -0500

    Time: 158 ms (15.96%), Space: 40.3 MB (62.02%) - LeetHub

--- 69-sqrtx/69-sqrtx.js
+++ 69-sqrtx/69-sqrtx.js
@@ -1,3 +1,30 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+    //cut x into half - lo, hi, mid
+    let lo = 1
+    let hi = Math.floor(x / 2) + 1
+    let mid
+    //while lo is less or equal to hi 
+    while (lo <= hi) {
+        mid = Math.floor( (lo + hi) / 2)
+        //calculate mid 
+
+        if (mid * mid > x) {
+            hi = mid - 1
+        } else if ( mid * mid < x ) {
+            lo = mid + 1 
+        } else {
+            return mid
+        }
+        
+    }
+  return hi 
+};
+
+
  var mySqrt = function(x) {
   var left = 1;
   var right = Math.floor(x / 2) + 1;
