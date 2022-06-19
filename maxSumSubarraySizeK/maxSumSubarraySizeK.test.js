74553a57ae51a06e19a57c1fd904f1653ad8f04f
swizzlestix09 <angely.rodriguez@gmail.com>
2022-06-18 21:21:30 -0400

    completed maxSumSubArraySzK w/ test

--- maxSumSubarraySizeK/maxSumSubarraySizeK.test.js
+++ maxSumSubarraySizeK/maxSumSubarraySizeK.test.js
@@ -2,7 +2,9 @@ const maxSumSubArraySzK = require('./maxSumSubarraySizeK');
 
 describe( 'Maximum Sum Subarray of Size K (easy)', () => {
  var testValues = [
- [ [2, 1, 5, 1, 3, 2], 3 , 9]
+ [ [2, 1, 5, 1, 3, 2], 3 , 9],
+ [ [1, 2, 4, 7, 8, 9, 9, 7, 5, 3, 2], 2 , 18],
+ [ [1], 2 , 0]
  ]
 test.each(testValues)( 'The function takes Array %j and K %d and produces result %d', (array, k, expected) => {
   expect(maxSumSubArraySzK(array, k)).toBe(expected)
