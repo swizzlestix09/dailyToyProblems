60bdb6077181926a70f6b057ce3362433f6a8d2c
swizzlestix09 <angely.rodriguez@gmail.com>
2022-06-20 22:40:45 -0400

    completed problem

--- subarrayGreatestSum/subarrayGreatestSum.js
+++ subarrayGreatestSum/subarrayGreatestSum.js
@@ -1,20 +1,39 @@
-
 /*
 Given an array of positive numbers and a positive number ‘S,’ find the length of the smallest contiguous subarray whose sum is greater than or equal to ‘S’. Return 0 if no such subarray exists.
 */
 
-
 const subarrayGreatestSum = (arr, target) => {
+  let arraySumLen = Infinity;
+  let startIdx = 0;
+  let movingIdx = 1;
 
-}
+  while (startIdx <= arr.length - 2) {
+    let sum = 0;
+    for (let i = startIdx; i < movingIdx; i++) {
+      sum += arr[i];
+    }
+    //console.log(sum)
+    if (sum >= target && (movingIdx - startIdx) < arraySumLen ) arraySumLen = (movingIdx - startIdx)
+    if (movingIdx === arr.length) {
+      startIdx++;
+      movingIdx = startIdx+1;
+    } else {
+      ++movingIdx;
+    }
+  }
+  return arraySumLen === Infinity ? 0 : arraySumLen;
+};
 
-//two var
-//let beginningIdx = 0
+//tree var
+//let arraySumLen = 0
+//let startIdx = 0
 //let movingIdx = 1
 //while beginning array is smaller than or equal to arr.len - 2
-//let arrayPortion = array.slice beginningIdx, movingIdx
-//
+//let sum = 0
+// for loop.. from start idx greater than or equal to moving idx
+//add arr[i]
+//if sum >= target arraySumLen = movingIdx - StartIdx
+//return arraySumLen
 
-
-console.log(subarrayGreatestSum([2, 1, 5, 2, 3, 2], 7))
-export default subarrayGreatestSum;
+// console.log(subarrayGreatestSum([2, 1, 5, 2, 3, 2], 7)); //2
+module.exports = subarrayGreatestSum;
--- subarrayGreatestSum/subarrayGreatestSum.test.js
+++ subarrayGreatestSum/subarrayGreatestSum.test.js
@@ -4,8 +4,10 @@ describe('Smallest Subarray with a Greater Sum (easy) - Grokk',()=>{
  var testParams = [
   [[2, 1, 5, 2, 3, 2], 7, 2],
   [ [2, 1, 5, 2, 8], 7, 1],
-  [[3, 4, 1, 1, 6], 8, 3]
- ]
+  [[3, 4, 1, 1, 6], 8, 3],
+  [[], 8, 0],
+  [[3, 4, 1, 1, 6], 25, 0],
+ ];
 
  test.each(testParams)('Expect %j and given target %d we expect: %d', (testArray, testMin, expected)=>{
   expect(subarrayGreatestSum(testArray, testMin)).toEqual(expected);
