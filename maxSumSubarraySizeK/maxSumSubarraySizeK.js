3dd488bb9ef754a85de098ca4f5c770b9ccae622
swizzlestix09 <angely.rodriguez@gmail.com>
2022-06-20 21:02:54 -0400

    redid maxSumSubarraySizeK to use sliding window approach

--- maxSumSubarraySizeK/maxSumSubarraySizeK.js
+++ maxSumSubarraySizeK/maxSumSubarraySizeK.js
@@ -1,22 +1,54 @@
 
+// const maxSumSubArraySzK = (array, k) => {
+//   if ( k > array.length ) return 0;
+//   let sumSubArr = 0;
+//   let begSubArr = 0;
+//   let endSubArr = k;
+//   while (begSubArr < endSubArr && endSubArr <= array.length) {
+//     let subArray = array.slice(begSubArr, endSubArr);
+//     let sum = subArray.reduce((a, b) => a + b, 0)
+//     sumSubArr = Math.max(sumSubArr, sum);
+//     ++begSubArr;
+//     ++endSubArr;
+//   }
+//   return sumSubArr;
+// };
+
 const maxSumSubArraySzK = (array, k) => {
-  if ( k > array.length ) return 0;
-  let sumSubArr = 0;
-  let begSubArr = 0;
-  let endSubArr = k;
-  while (begSubArr < endSubArr && endSubArr <= array.length) {
-    let subArray = array.slice(begSubArr, endSubArr);
-    let sum = subArray.reduce((a, b) => a + b, 0)
-    sumSubArr = Math.max(sumSubArr, sum);
-    ++begSubArr;
-    ++endSubArr;
+  let tempSubSum = 0;
+  let finalSum = 0;
+
+
+  for ( let i = 0; i < k; i++) {
+    tempSubSum += array[i];
   }
-  return sumSubArr;
+
+  let addIdx = k;
+  let subIdx = 0;
+
+  while(addIdx <= array.length - 1) {
+    tempSubSum +=  array[addIdx] - array[subIdx];
+    console.log(array, 'temp', tempSubSum);
+    finalSum = Math.max(finalSum, tempSubSum);
+    ++subIdx, ++addIdx;
+  }
+
+  return finalSum;
 };
+
+
+// add 0 to n together
+//two vars = subIdx = 0, addIdx = k + 1
+//while addIdx <= array.length
+// subtract array[subIdx] from maxSubSum and add array[addIdx]
+// ++ subIdx and ++addIdx
+// check is tempSubSum is greater than finalSum, if so replace
+// once iterations complete return finalSum.
+
 
 module.exports = maxSumSubArraySzK;
 
-
+console.log(maxSumSubArraySzK([2, 1, 5, 1, 3, 2], 3))
 
 //var beggining sub array
 //var end sub array
