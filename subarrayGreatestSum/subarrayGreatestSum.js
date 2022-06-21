0d8ec84184955d99ba361e3f4a030356a4f27122
swizzlestix09 <angely.rodriguez@gmail.com>
2022-06-21 19:19:18 -0400

    edits

--- subarrayGreatestSum/subarrayGreatestSum.js
+++ subarrayGreatestSum/subarrayGreatestSum.js
@@ -12,7 +12,6 @@ const subarrayGreatestSum = (arr, target) => {
     for (let i = startIdx; i < movingIdx; i++) {
       sum += arr[i];
     }
-    //console.log(sum)
     if (sum >= target && (movingIdx - startIdx) < arraySumLen ) arraySumLen = (movingIdx - startIdx)
     if (movingIdx === arr.length) {
       startIdx++;
@@ -23,17 +22,6 @@ const subarrayGreatestSum = (arr, target) => {
   }
   return arraySumLen === Infinity ? 0 : arraySumLen;
 };
-
-//tree var
-//let arraySumLen = 0
-//let startIdx = 0
-//let movingIdx = 1
-//while beginning array is smaller than or equal to arr.len - 2
-//let sum = 0
-// for loop.. from start idx greater than or equal to moving idx
-//add arr[i]
-//if sum >= target arraySumLen = movingIdx - StartIdx
-//return arraySumLen
 
 // console.log(subarrayGreatestSum([2, 1, 5, 2, 3, 2], 7)); //2
 module.exports = subarrayGreatestSum;
