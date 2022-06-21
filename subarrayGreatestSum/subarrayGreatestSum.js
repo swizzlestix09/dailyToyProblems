fbc3d46045e8ab304145bd0ccfdd1933f9b4d643
swizzlestix09 <angely.rodriguez@gmail.com>
2022-06-20 22:17:12 -0400

    psuedocoding solution

--- subarrayGreatestSum/subarrayGreatestSum.js
+++ subarrayGreatestSum/subarrayGreatestSum.js
@@ -1,6 +1,20 @@
 
-const subarrayGreatestSum = () => {
+/*
+Given an array of positive numbers and a positive number ‘S,’ find the length of the smallest contiguous subarray whose sum is greater than or equal to ‘S’. Return 0 if no such subarray exists.
+*/
+
+
+const subarrayGreatestSum = (arr, target) => {
 
 }
 
+//two var
+//let beginningIdx = 0
+//let movingIdx = 1
+//while beginning array is smaller than or equal to arr.len - 2
+//let arrayPortion = array.slice beginningIdx, movingIdx
+//
+
+
+console.log(subarrayGreatestSum([2, 1, 5, 2, 3, 2], 7))
 export default subarrayGreatestSum;
--- subarrayGreatestSum/subarrayGreatestSum.test.js
+++ subarrayGreatestSum/subarrayGreatestSum.test.js
@@ -1,1 +1,13 @@
 var subarrayGreatestSum = require('./subarrayGreatestSum')
+
+describe('Smallest Subarray with a Greater Sum (easy) - Grokk',()=>{
+ var testParams = [
+  [[2, 1, 5, 2, 3, 2], 7, 2],
+  [ [2, 1, 5, 2, 8], 7, 1],
+  [[3, 4, 1, 1, 6], 8, 3]
+ ]
+
+ test.each(testParams)('Expect %j and given target %d we expect: %d', (testArray, testMin, expected)=>{
+  expect(subarrayGreatestSum(testArray, testMin)).toEqual(expected);
+ })
+});
